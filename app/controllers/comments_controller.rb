fe13dadde95821e5a4f1b706585c6c5883ec3794
Marco Giuliani <thebaffo@gmail.com>
2021-02-11 16:10:08 +0700

    Initial commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,57 @@
+class CommentsController < ApplicationController
+  before_action :set_article, only: [:index, :create]
+  before_action :set_comment, only: [:show, :update, :destroy]
+
+  # GET /articles/{article_id}/comments
+  def index
+    @comments = @article.comments
+
+    render json: @comments
+  end
+
+  # GET /articles/{article_id}/comments/1
+  def show
+    render json: @comment
+  end
+
+  # POST /articles/{article_id}/comments
+  def create
+    @comment = @article.comments.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: [@article, @comment]
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/{article_id}/comments/1
+  def update
+    if @comment.update(comment_params)
+      render json: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /articles/{article_id}rai/comments/1
+  def destroy
+    @comment.destroy
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:article_id])
+    end
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_comment
+      @article = Article.find(params[:article_id])
+      @comment = Comment.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def comment_params
+      params.require(:comment).permit(:commenter, :body)
+    end
+end
---
+++ test/controllers/comments_controller_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+class CommentsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @comment = comments(:one)
+    @article = Article.find(@comment.article_id)
+  end
+
+  test "should get index" do
+    get article_comments_url(@article), as: :json
+    assert_response :success
+  end
+
+  test "should create comment" do
+    assert_difference('Comment.count') do
+      post article_comments_url(@article), params: { comment: { body: @comment.body, commenter: @comment.commenter } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show comment" do
+    get article_comment_url(@article, @comment), as: :json
+    assert_response :success
+  end
+
+  test "should update comment" do
+    patch article_comment_url(@article, @comment), params: { comment: { body: @comment.body, commenter: @comment.commenter } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy comment" do
+    assert_difference('Comment.count', -1) do
+      delete article_comment_url(@article, @comment), as: :json
+    end
+
+    assert_response 204
+  end
+end
